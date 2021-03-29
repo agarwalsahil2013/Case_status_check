adbbd4775196f2c478654506b9da7f12f529b030
agarwalsahil2013 <52674385+agarwalsahil2013@users.noreply.github.com>
2021-03-29 10:54:03 -0500

    Update case_status.py

--- case_status.py
+++ case_status.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+import os
+# from webdriver_manager.chrome import ChromeDriverManager
 from twilio.rest import Client
 #import pandas as pd
 #from waitress import serve
@@ -13,13 +14,14 @@ client = Client(account_sid, auth_token)
 url = "https://egov.uscis.gov/casestatus/landing.do"
 
 options = Options()
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("enable-automation")
 options.add_argument("--disable-infobars")
 options.add_argument("--disable-dev-shm-usage")
-chrome_browser = webdriver.Chrome(executable_path=ChromeDriverManager().install(),options=options)
+chrome_browser = webdriver.Chrome((executable_path=os.environ.get("CHROMEDRIVER_PATH"),options=options)
 
 # chrome_browser = webdriver.Chrome('./chromedriver')
 # chrome_browser.get(url)
